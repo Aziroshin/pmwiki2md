9e592c70e19cd5e0750b82bcaf4524243529d65e
Aziroshin <aziroshin@gmx.net>
2019-04-25 20:47:12 +0200

    Changed paradigm yet again. We're now working with a tree of processed and yet-to-process elements, whereas every element that's being processed in a conversion's .convert method is split into sub-elements at found indicators and re-inserted into the tree with the indicators (as distinct elements) surrounding them in the tree-list. The only conversion currently being used to prototype this is ConversionBySingleCodeReplacement.

--- pmwiki2md.py
+++ pmwiki2md.py
@@ -7,6 +7,7 @@
 
 # Python
 from collections import UserList, namedtuple
+import copy
 
 #=======================================================================================
 # Named Tuples
@@ -39,8 +40,36 @@ class ContentElement(object):
 		self.availableForConversion = availableForConversion
 
 class Content(UserList):
-	def __init__(self, contentToConvert):
-		self.data = [ContentElement(contentToConvert)]
+	
+	def __init__(self, initialData=[]):
+		if type(initialData) == list:
+			self.data = initialData
+		elif type(initialData) == UserList:
+			self.data = initialData.data
+		else:
+			self.data = [ContentElement(initialData)]
+	
+	def getElementIndex(self, element):
+		
+		"""Returns the index of the specified ContentElement object."""
+		
+		index = 0
+		for prospectiveElement in self.data:
+			if prospectiveElement == element:
+				return index
+			index += 1
+		raise IndexError("Tried to get index of ContentElement object not in list.")
+	
+	def replaceElement(self, element, replacementElements):
+		"""Replace the specified ContentElement object with a list of ContentElement objects."""
+		index = self.getElementIndex(element)
+		self.pop(index)
+		for replacementElement in reversed(replacementElements):
+			self.insert(index, replacementElement)
+	def copy(self):
+		new = self.__class__()
+		[new.append(element) for element in self.data]
+		return new
 
 class ConvertibleDocument(object): 
 	
@@ -58,7 +87,15 @@ class Conversion(object):
 	
 	def convert(self):
 		pass#Override
-		
+
+class ConversionSubElementsprocessor(object):
+	def getSubElements(self, subElements):
+		pass#OVERRIDE
+
+class ConversionLeftAdjacentSubElementsProcessor(object):
+	def getSubElements(self, element):
+		element.content.split(self.old)
+
 class ConversionBySingleCodeReplacement(Conversion):
 	
 	@property
@@ -69,12 +106,31 @@ class ConversionBySingleCodeReplacement(Conversion):
 	def new(self):
 		return self.__class__.NEW
 	
+	def getSubElements(self, element):
+		return [ContentElement(subElement) for subElement in element.content.split(self.old)]
+	
+	def interleaveWithConvertedIndicators(self, subElements):
+		convertedSubElements = []
+		for subElement in subElements:
+			convertedSubElements.append(subElement)
+			convertedSubElements.append(ContentElement(self.new, availableForConversion=False))
+		# To simulate proper "".join() behaviour, cut off the excess we've likely added.
+		# In case the content element in question ended with a formatting indicator, however,
+		# the last sub element will be '', as a result of the the behaviour of "".split.
+		if convertedSubElements[-1].content == self.new:
+			convertedSubElements.pop()
+		return convertedSubElements
+	
 	def convert(self, content):
+		alteredContent = content.copy()
 		for element in content:
+			print([c.content for c in alteredContent])
 			if element.availableForConversion:
-				element.content = element.content.replace(self.old, self.new)
-				element.conversions.append(self)
-		
+				subElements = self.getSubElements(element)
+				convertedSubElements = self.interleaveWithConvertedIndicators(subElements)
+				alteredContent.replaceElement(element, convertedSubElements)
+		return alteredContent
+
 class Conversions(UserList):
 	
 	def __init__(self, *conversions):
@@ -85,9 +141,32 @@ class Conversions(UserList):
 #==========================================================
 
 class Pmwiki2MdItalicConversion(ConversionBySingleCodeReplacement):
-	
-	"""Conversion of italic content elements from Pm2Wiki to markdown."""
-	
 	OLD = "''"
-	NEW = "*"
-	
+	NEW = "_"
+class Pmwiki2MdTitle1Conversion(ConversionBySingleCodeReplacement):
+	OLD = "!"
+	NEW = "#"
+class Pmwiki2MdTitle2Conversion(ConversionBySingleCodeReplacement):
+	OLD = "!!"
+	NEW = "##"
+class Pmwiki2MdTitle3Conversion(ConversionBySingleCodeReplacement):
+	OLD = "!!!"
+	NEW = "###"
+class Pmwiki2MdListConversion(ConversionBySingleCodeReplacement):
+	OLD = "*"
+	NEW = "* "
+class Pmwiki2MdList2Conversion(ConversionBySingleCodeReplacement):
+	OLD = "**"
+	NEW = "  * "
+class Pmwiki2MdList3Conversion(ConversionBySingleCodeReplacement):
+	OLD = "***"
+	NEW = "    * "
+class Pmwiki2MdDoubleNewlineConversion(ConversionBySingleCodeReplacement):
+	OLD = "\\"
+	NEW = "\n\n"
+class Pmwiki2MdCodeBlockConversion(Conversion):
+	def convert(self):
+		pass#TODO
+class Pmwiki2MdLinkConversion(Conversion):
+	def convert(self):
+		pass#TODO
--- tests/unit/pmwiki2md.py
+++ tests/unit/pmwiki2md.py
@@ -19,10 +19,20 @@ from pmwiki2md import Content, Pmwiki2MdItalicConversion, Conversions
 class ConversionTests(unittest.TestCase):
 	
 	def test_Pmwiki2MdItalicConversion(self):
-		content = Content("Cucumbers ''might'' be tomatoes if they were not already cucumbers.")
+		original = Content("Cucumbers ''might'' be tomatoes.")
+		shouldLookLike = ["Cucumbers ", "_", "might", "_", " be tomatoes."]
 		conversions = Conversions(Pmwiki2MdItalicConversion)
-		converted = Pmwiki2MdItalicConversion().convert(content)
-		self.assertEqual(content[0].content, "Cucumbers *might* be tomatoes if they were not already cucumbers.")
+		converted = Pmwiki2MdItalicConversion().convert(original)
+		dprint("\n")
+		dprint([c.content for c in original])
+		dprint("\n")
+		dprint(shouldLookLike)
+		dprint("\n")
+		dprint([c.content for c in converted])
+		dprint("\n")
+		#self.assertEqual(content[0].content, "Cucumbers _might_ be tomatoes if they were not already cucumbers.")
+		for lookalike in shouldLookLike:
+			self.assertEqual(converted.pop(0).content, lookalike)
 
 #=======================================================================================
 	
