c04642396180cb10509bf5fb5b9ae97bf8466dd4
Aziroshin <aziroshin@gmx.net>
2019-04-25 21:24:49 +0200

    Rearranged tests some more, added convenient debugging function cdprint and the beginnings of a test for Pmwiki2MdTitle1Conversion, which, I think, doesn't check all it needs yet.

--- tests/unit/pmwiki2md.py
+++ tests/unit/pmwiki2md.py
@@ -12,23 +12,37 @@ from lib.debugging import dprint
 import time
 
 #=======================================================================================
+# Specific Debugging Tools
+#=======================================================================================
+
+def cdprint(content):
+	"""Print .content of the ContentElement objs. in the specified Content obj."""
+	dprint([contentElement.content for contentElement in content])
+
+#=======================================================================================
 # Tests
 #=======================================================================================
 
 from pmwiki2md import Content, Conversions
 class ConversionTests(unittest.TestCase):
 	
-	def compareConverted(self, converted, shouldLookLike):
+	def compareConverted(self, original, shouldLookLike, conversion):
+		"""Converts and compares the result to what it should look like."""
+		converted = conversion().convert(original)
 		for lookalike in shouldLookLike:
 			self.assertEqual(converted.pop(0).content, lookalike)
 	
 	def test_Pmwiki2MdItalicConversion(self):
-		from pmwiki2md import Pmwiki2MdItalicConversion
+		from pmwiki2md import Pmwiki2MdItalicConversion as conversion
 		original = Content("Cucumbers ''might'' be tomatoes.")
 		shouldLookLike = ["Cucumbers ", "_", "might", "_", " be tomatoes."]
-		conversions = Conversions(Pmwiki2MdItalicConversion)
-		converted = Pmwiki2MdItalicConversion().convert(original)
-		self.compareConverted(converted, shouldLookLike)
+		self.compareConverted(original, shouldLookLike, conversion)
+	
+	def test_Pmwiki2MdTitle1Conversion(self):
+		from pmwiki2md import Pmwiki2MdTitle1Conversion as conversion
+		original = Content("!Cucumbers might be tomatoes.")
+		shouldLookLike = ["", "#", "Cucumbers might be tomatoes."]
+		self.compareConverted(original, shouldLookLike, conversion)
 
 #=======================================================================================
 	
