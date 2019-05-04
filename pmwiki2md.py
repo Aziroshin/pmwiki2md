ae272fee313b4dc02c4049a7f0cf40a22e9ae134
Aziroshin <aziroshin@gmx.net>
2019-05-04 10:09:57 +0200

    Abandoned Content bug and put in place hackaround for now; test_Pmwiki2Md1LevelBulletListConversion passes.

--- pmwiki2md.py
+++ pmwiki2md.py
@@ -45,16 +45,29 @@ class ContentElement(object):
 
 class Content(UserList):
 	
-	def __init__(self, initialData=[]):
-		dprint("initialData type: ", type(initialData))
-		if type(initialData) == list:
-			dprint("initialData is list: ", initialData)
+	def __init__(self, initialData=None):
+		#dprint("initialData type: ", type(initialData))
+		if initialData is None:
+			# NOTE: This is a hackaround for an unhandled bug.
+			#
+			# If the parameter for initial data is configured as initialData=[],
+			# it somehow ends up taking self.data from another instance of the
+			# class. There's something I'm missing, and this works around it
+			# for now. It's not graceful, but I won't stall progress on the
+			# project any longer for a wild goose chase. Perhaps another time.
+			#
+			# If there is any further weirdness of the sort, this is probably
+			# a good starting point.
+			#dprint("initialData is None: ", initialData)
+			self.data = []
+		elif type(initialData) is list:
+			#dprint("initialData is list: ", initialData)
 			self.data = initialData
-		elif type(initialData) == UserList:
-			dprint("initialData is UserList: ", initialData)
+		elif type(initialData) is UserList:
+			#dprint("initialData is UserList: ", initialData)
 			self.data = initialData.data
 		else:
-			dprint("initialData is something else: ", initialData)
+			#dprint("initialData is something else: ", initialData)
 			self.data = [ContentElement(initialData)]
 	
 	def getElementIndex(self, element):
@@ -75,17 +88,23 @@ class Content(UserList):
 		for replacementElement in reversed(replacementElements):
 			self.insert(index, replacementElement)
 	def copy(self):
+		#dprint("will see initial data messages:")
 		new = self.__class__()
-		dprint("self, self.data, new, class, class: \n", self, "\n", self.data, "\n", new, "\n", Content(), "\n", Content())
+		#dprint("End of initial data messages.")
+		#dprint("my data id:\t\t", id(self.data))
+		#dprint("new data id:\t\t", id(new.data))
+		#dprint("my id:\t\t", id(self))
+		#dprint("new id:\t\t", id(new))
+		#dprint("self, self.data, new, class, class: \n", self, "\n", self.data, "\n", new, "\n", Content(), "\n", Content())
 
-		i = 0
+		#i = 0
 		for element in self.data:
 			new.append(element)
-			#print(len(self.data))
-			i += 1
-			if i == 10:
-				#raise Exception("INFINITE LOOP")
-				time.sleep(100000000)
+			#print(self.data)
+			#i += 1
+			#if i == 10:
+			#	#raise Exception("INFINITE LOOP")
+			#	time.sleep(100000000)
 		
 		#[dprint("Copying forever?"+str(new.append(element))) for element in self.data]
 		#dprint("REMOVE ABOVE LINE AND UNCOMMENT LINE BELOW.")
@@ -131,11 +150,12 @@ class ConversionBySingleCodeReplacement(Conversion):
 		return convertedSubElements
 	
 	def convert(self, content):
-		dprint("Content class comparison in super().convert before copy: ", Content(), Content())
-		dprint("content object id before copy: ", id(content))
+		#dprint("Content before copying: ", content)
+		#dprint("Content class comparison in super().convert before copy: ", Content(), Content())
+		#dprint("content object id before copy: ", id(content))
 		alteredContent = content.copy()
-		dprint("content alteredContent, content ids after copy: ", id(alteredContent), id(content))
-		dprint("Content class comparison in super().convert after copy: ", Content(), Content())
+		#dprint("content alteredContent, content ids after copy: ", id(alteredContent), id(content))
+		#dprint("Content class comparison in super().convert after copy: ", Content(), Content())
 		for element in content:
 			if element.availableForConversion:
 				subElements = self.getSubElements(element)
@@ -159,7 +179,7 @@ class ListConversion(ConversionBySingleCodeReplacement):
 		
 		# Indent the markdown list indicator by two spaces per list level.
 		level = 1
-		convertedContent = content
+		contentBeforeConversion = content
 		while True:
 			indentedNew = "".join(["  " for level in range(0, level)]+[self.new])
 			theNewOld = os.linesep+self.old+" "
@@ -170,22 +190,20 @@ class ListConversion(ConversionBySingleCodeReplacement):
 			self.new = theNewNew
 			
 			# Our parent class can take over.
-			dprint("content before super()", convertedContent)
-			dprint("Content class comparison before super(): ", Content(), Content())
-			convertedContent = super().convert(convertedContent)
-			dprint("content after super()", convertedContent)
-			dprint("Content class comparison after super(): ", Content(), Content())
-			cdprint(convertedContent)
+			#dprint("content before super()", convertedContent)
+			#dprint("Content class comparison before super(): ", Content(), Content())
+			convertedContent = super().convert(contentBeforeConversion)
+			#dprint("content after super()", convertedContent)
+			#dprint("Content class comparison after super(): ", Content(), Content())
+			#cdprint(contentBeforeConversion)
+			#cdprint(convertedContent)
 			# There might be a better way to determine that
 			# there are no lists of any greater levels anymore.
 			# Right now, we're just looking at whether there
 			# was anything to convert during the last pass.
-			print("LALA1")
-			if len(convertedContent) == len(content):
-				print("LALA BREAK")
+			if len(convertedContent) == len(contentBeforeConversion):
 				break
-			print("LALA2")
-		print("LALA3")
+			contentBeforeConversion = convertedContent
 		return convertedContent
 
 class Conversions(UserList):
--- tests/unit/pmwiki2md.py
+++ tests/unit/pmwiki2md.py
@@ -24,44 +24,44 @@ class ConversionTests(unittest.TestCase):
 		for lookalike in shouldLookLike:
 			self.assertEqual(converted.pop(0).content, lookalike)
 	
-	#def test_Pmwiki2MdItalicConversion(self):
-		#from pmwiki2md import Pmwiki2MdItalicConversion as conversion
-		#original = Content("Cucumbers ''might'' be tomatoes.")
-		#shouldLookLike = ["Cucumbers ", "_", "might", "_", " be tomatoes."]
-		#self.compareConverted(original, shouldLookLike, conversion)
+	def test_Pmwiki2MdItalicConversion(self):
+		from pmwiki2md import Pmwiki2MdItalicConversion as conversion
+		original = Content("Cucumbers ''might'' be tomatoes.")
+		shouldLookLike = ["Cucumbers ", "_", "might", "_", " be tomatoes."]
+		self.compareConverted(original, shouldLookLike, conversion)
 	
-	#def test_Pmwiki2MdBoldConversion(self):
-		#from pmwiki2md import Pmwiki2MdBoldConversion as conversion
-		#original = Content("Cucumbers '''might''' be tomatoes.")
-		#shouldLookLike = ["Cucumbers ", "__", "might", "__", " be tomatoes."]
-		#self.compareConverted(original, shouldLookLike, conversion)
+	def test_Pmwiki2MdBoldConversion(self):
+		from pmwiki2md import Pmwiki2MdBoldConversion as conversion
+		original = Content("Cucumbers '''might''' be tomatoes.")
+		shouldLookLike = ["Cucumbers ", "__", "might", "__", " be tomatoes."]
+		self.compareConverted(original, shouldLookLike, conversion)
 		
-	#def test_Pmwiki2MdItalicBoldConversion(self):
-		#from pmwiki2md import Pmwiki2MdItalicBoldConversion as conversion
-		#original = Content("Cucumbers '''''might''''' be tomatoes.")
-		#shouldLookLike = ["Cucumbers ", "**_", "might", "**_", " be tomatoes."]
-		#self.compareConverted(original, shouldLookLike, conversion)
+	def test_Pmwiki2MdItalicBoldConversion(self):
+		from pmwiki2md import Pmwiki2MdItalicBoldConversion as conversion
+		original = Content("Cucumbers '''''might''''' be tomatoes.")
+		shouldLookLike = ["Cucumbers ", "**_", "might", "**_", " be tomatoes."]
+		self.compareConverted(original, shouldLookLike, conversion)
 	
-	#def test_Pmwiki2MdTitle1Conversion(self):
-		#from pmwiki2md import Pmwiki2MdTitle1Conversion as conversion
-		#original = Content("\n! Cucumbers might be tomatoes.")
-		#shouldLookLike = ["", "\n# ", "Cucumbers might be tomatoes."]
-		#self.compareConverted(original, shouldLookLike, conversion)
+	def test_Pmwiki2MdTitle1Conversion(self):
+		from pmwiki2md import Pmwiki2MdTitle1Conversion as conversion
+		original = Content("\n! Cucumbers might be tomatoes.")
+		shouldLookLike = ["", "\n# ", "Cucumbers might be tomatoes."]
+		self.compareConverted(original, shouldLookLike, conversion)
 
-	#def test_Pmwiki2Md1LevelBulletListConversion(self):
-		#from pmwiki2md import Pmwiki2MdBulletListConversion as conversion
-		#original = Content("\n* Cucumbers might be tomatoes.")
-		#shouldLookLike = ["", "\n* ", "Cucumbers might be tomatoes."]
-		#self.compareConverted(original, shouldLookLike, conversion)
+	def test_Pmwiki2Md1LevelBulletListConversion(self):
+		from pmwiki2md import Pmwiki2MdBulletListConversion as conversion
+		original = Content("\n* Cucumbers might be tomatoes.")
+		shouldLookLike = ["", "\n  - ", "Cucumbers might be tomatoes."]
+		self.compareConverted(original, shouldLookLike, conversion)
 		
-	def test_Content(self):
+	def test_ContentCopy(self):
 		print("")
 		from pmwiki2md import Content
 		content1 = Content()
 		content1.append("a")
 		content1Copy = content1.copy()
 		content2 = Content()
-		self.assertEqual(content1, [])
+		self.assertEqual(content1, ["a"])
 		self.assertEqual(content2, [])
 
 #=======================================================================================
